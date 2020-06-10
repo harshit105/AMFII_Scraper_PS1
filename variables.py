09e04ad2c97476109b23ecdc04fd9593aba382fa
harshit105 <46093097+harshit105@users.noreply.github.com>
2020-06-10 18:07:46 +0530

    updated month_dictionary

--- variables.py
+++ variables.py
@@ -18,9 +18,10 @@ year=[x.text for x in yroptions]
 
 #month_dictionary- Add new month after every 1 month manually
 month_dictionary={
+    '2020-2021':['June-2020','May-2020','April-2020'],
+    '2019-2020':['March-2020','February-2020','January-2020','December-2019','November-2019','October-2019','September-2019','August-2019','July-2019','June-2019','May-2019','April-2019'],
     '2018-2019':['March-2019','February-2019','January-2019','December-2018','November-2018','October-2018','September-2018','August-2018','July-2018','June-2018','May-2018','April-2018','January-2018'],
-    '2019-2020':['April-2020','March-2020','February-2020','January-2020','December-2019','November-2019','October-2019','September-2019','August-2019','July-2019','June-2019','May-2019','April-2019'],
-    '2020-2021':['June-2020','May-2020','April-2020']
+    
 }
 
 
